9f65573c6d538f19793049a1619901df63ef1307
allison sharpe <allisonsharpe@ymail.com>
2016-08-16 14:38:29 -0400

    modified files

--- update_page.php
+++ update_page.php
@@ -4,7 +4,7 @@ echo $id = $_POST['id'];
 echo $title = $_POST['title'];
 echo $entry_date = $_POST['entry_date'];
 echo $content = $_POST['content'];
-$db = mysqli_connect("localhost", "root", "") or die ("Could not connect to database");
+$db = mysqli_connect("45.55.177.160", "root", "") or die ("Could not connect to database");
 mysqli_select_db($db, "entries") or die ("Could not find database");
 $sql = "UPDATE new_entry SET title='$title', entry_date='$entry_date', content='$content' WHERE id= $id";
 $result = mysqli_query($db, $sql);
